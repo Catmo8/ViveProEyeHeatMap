6b2a1fd0a13f11280463a5580bb13a5e214f2e20
Frank J Fontana <ffonta2@lsu.edu>
2019-10-18 14:37:43 -0500

    messed with heatmap

--- CSC 4356 Project/Assets/heatmaps/Heatmap.cs
+++ CSC 4356 Project/Assets/heatmaps/Heatmap.cs
@@ -16,18 +16,22 @@ public class Heatmap : MonoBehaviour
     {
         positions = new Vector4[count];
         properties = new Vector4[count];
-
+        /*
         for (int i = 0; i < positions.Length; i++)
         {
             positions[i] = new Vector4(Random.Range(-0.4f, +0.4f), Random.Range(-0.4f, +0.4f), 0, 0);
             properties[i] = new Vector4(Random.Range(0f, 0.25f), Random.Range(-0.25f, 1f), 0, 0);
         }
+        */
     }
 
     void Update()
     {
         for (int i = 0; i < positions.Length; i++)
+        {
             positions[i] += new Vector4(Random.Range(-0.1f, +0.1f), Random.Range(-0.1f, +0.1f), 0, 0) * Time.deltaTime;
+            properties[i] = new Vector4(Random.Range(0f, 0.25f), Random.Range(-0.25f, 1f), 0, 0);
+        }
 
         material.SetInt("_Points_Length", count);
         material.SetVectorArray("_Points", positions);
