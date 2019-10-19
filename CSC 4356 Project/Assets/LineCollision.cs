0ca8ca70492d20f545702e41eede317b790b623a
Frank J Fontana <ffonta2@lsu.edu>
2019-10-18 19:57:05 -0500

    Figured out how the shader works.

---
+++ CSC 4356 Project/Assets/LineCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCollision : MonoBehaviour
+{
+    LineRenderer line;
+    public static bool colliding;
+    public static Ray laserRay;
+    public static RaycastHit colliderInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        colliding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set up a ray, and make it's direction go forward
+        laserRay = new Ray(line.GetPosition(0), line.GetPosition(1));
+        //If our ray hits something, we want to set the next index of the line renderer to be the exact vector3 where the ray hit, which you can get by
+        if (Physics.Raycast(laserRay, out colliderInfo, Mathf.Infinity))
+        {
+            line.SetPosition(1, colliderInfo.point);
+            colliding = true;
+        }
+        else
+        {
+            colliding = false;
+        }
+
+    }
+}
--- CSC 4356 Project/Assets/heatmaps/Heatmap.cs
+++ CSC 4356 Project/Assets/heatmaps/Heatmap.cs
@@ -7,15 +7,25 @@ public class Heatmap : MonoBehaviour
 {
     public Vector4[] positions;
     public Vector4[] properties;
+    public float[] radiuses;
+    public float[] intensities;
 
+    public LineRenderer line;
     public Material material;
 
     public int count = 50;
+    int i = 0;
+
+
 
     void Start()
     {
         positions = new Vector4[count];
         properties = new Vector4[count];
+        radiuses = new float[count];
+        intensities = new float[count];
+
+        InvokeRepeating("SetNewPos", 1f, 1f);
         /*
         for (int i = 0; i < positions.Length; i++)
         {
@@ -23,18 +33,42 @@ public class Heatmap : MonoBehaviour
             properties[i] = new Vector4(Random.Range(0f, 0.25f), Random.Range(-0.25f, 1f), 0, 0);
         }
         */
+        /*
+        for (int j = 0; j < positions.Length; j++)
+        {
+            positions[j] = new Vector4(Random.Range(-0.4f, +0.4f), Random.Range(-0.4f, +0.4f));
+            radiuses[j] = Random.Range(0f, 0.25f);
+            intensities[j] = Random.Range(-0.25f, 1f);
+            properties[j] = new Vector4(radiuses[j], intensities[j], 0, 0);
+        }
+        */
     }
 
     void Update()
     {
-        for (int i = 0; i < positions.Length; i++)
-        {
-            positions[i] += new Vector4(Random.Range(-0.1f, +0.1f), Random.Range(-0.1f, +0.1f), 0, 0) * Time.deltaTime;
-            properties[i] = new Vector4(Random.Range(0f, 0.25f), Random.Range(-0.25f, 1f), 0, 0);
-        }
-
-        material.SetInt("_Points_Length", count);
+        material.SetInt("_Points_Length", positions.Length);
         material.SetVectorArray("_Points", positions);
         material.SetVectorArray("_Properties", properties);
+        if (i == count)
+        {
+            CancelInvoke("SetNewPos");
+        }
+    }
+    
+    void SetNewPos()
+    {
+        if (LineCollision.colliding == true && i < count)
+        {
+            Debug.Log("Hit");
+            //positions[i] += new Vector4(Random.Range(-0.1f, +0.1f), Random.Range(-0.1f, +0.1f), 0, 0) * Time.deltaTime;
+            positions[i] += new Vector4(LineCollision.colliderInfo.point.x, LineCollision.colliderInfo.point.y, 0, 0) * Time.deltaTime;
+            properties[i] += new Vector4(LineCollision.colliderInfo.point.x, LineCollision.colliderInfo.point.y, 0, 0) * Time.deltaTime;
+            /*
+            material.SetInt("_Points_Length", count);
+            material.SetVectorArray("_Points", positions);
+            material.SetVectorArray("_Properties", properties);
+            */
+            i++;
+        }
     }
 }
